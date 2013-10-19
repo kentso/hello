52e86d00f2ba296cdbabff8895dc99dbf336901e
Ken Tso <kytso@cse.cuhk.edu.hk>
2013-10-19 13:52:24 +0000

    fixed bug: wrong assignment

--- www/js/index.js
+++ www/js/index.js
@@ -4,10 +4,15 @@ var sqlite_db =  function () {
     var db;
 
     return {
-        init : function(){
+        // Transaction error callback
+        errorCB : function (err) {
+            console.log("Error processing SQL: " + err.code);
+        },
+
+        init : function () {
             db = window.openDatabase("Database", "1.0", "photo and tag", 10000000);
             db.transaction(
-                function(tx){
+                function (tx) {
                     tx.executeSql('CREATE TABLE IF NOT EXISTS photo (path TEXT PRIMARY KEY, create_date INTEGER, tag TEXT)');
                     tx.executeSql('CREATE TABLE IF NOT EXISTS tag (name TEXT PRIMARY KEY, last_update INTEGER)');
                     tx.executeSql('SHOW TABLES');
@@ -19,7 +24,7 @@ var sqlite_db =  function () {
         insert_photo : function (path, create_date, tag) {
             var tag_arr = tag.split(',');
             db.transaction(
-                function(tx){
+                function(tx) {
                     tx.executeSql('INSERT INTO photo (path, create_date, tag) VALUES (' + path + ',' + create_date + ',' + tag + ')');
                     for ( var i=0 ; i < tag_arr.length ; i++ ){
                         tx.executeSql('INSERT OR REPLACE INTO tag (name, last_update) VALUES (' + tag_arr[i] + ',' + create_date + ')');
@@ -40,7 +45,7 @@ var sqlite_db =  function () {
                 },
                 errorCB
             );
-        },
+        }
 
        // // Query the database
        // function queryDB(tx) {
@@ -55,11 +60,6 @@ var sqlite_db =  function () {
        //         console.log("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data);
        //     }
        // }
-
-        // Transaction error callback
-        errorCB = function (err) {
-            console.log("Error processing SQL: "+err.code);
-        }
 
     };
 };
