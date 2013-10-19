9d2fcaa744a7bb4312df81b6e9272dcd4abc979c
Ken Tso <kytso@cse.cuhk.edu.hk>
2013-10-19 13:37:23 +0000

    sqlite database init

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,87 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var sqlite_db =  function () {
+
+    //10MB
+    var db;
+
+    return {
+        init : function(){
+            db = window.openDatabase("Database", "1.0", "photo and tag", 10000000);
+            db.transaction(
+                function(tx){
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS photo (path TEXT PRIMARY KEY, create_date INTEGER, tag TEXT)');
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS tag (name TEXT PRIMARY KEY, last_update INTEGER)');
+                    tx.executeSql('SHOW TABLES');
+                },
+                errorCB
+            );
+        },
+
+        insert_photo : function (path, create_date, tag) {
+            var tag_arr = tag.split(',');
+            db.transaction(
+                function(tx){
+                    tx.executeSql('INSERT INTO photo (path, create_date, tag) VALUES (' + path + ',' + create_date + ',' + tag + ')');
+                    for ( var i=0 ; i < tag_arr.length ; i++ ){
+                        tx.executeSql('INSERT OR REPLACE INTO tag (name, last_update) VALUES (' + tag_arr[i] + ',' + create_date + ')');
+                    }
+                },
+                errorCB
+            );
+        },
+
+        clear_db : function () {
+            db.transaction(
+                function(tx){
+                    tx.executeSql('DROP TABLE IF EXISTS DEMO');
+                    tx.executeSql('DROP TABLE IF EXISTS photo');
+                    tx.executeSql('DROP TABLE IF EXISTS tag');
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS photo (path TEXT PRIMARY KEY, create_date INTEGER, tag TEXT)');
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS tag (name TEXT PRIMARY KEY, last_update INTEGER)');
+                },
+                errorCB
+            );
+        },
+
+       // // Query the database
+       // function queryDB(tx) {
+       //     tx.executeSql('SELECT * FROM photo', [], querySuccess, errorCB);
+       // }
+
+       // // Query the success callback
+       // function querySuccess(tx, results) {
+       //     var len = results.rows.length;
+       //     console.log("DEMO table: " + len + " rows found.");
+       //     for (var i=0; i<len; i++){
+       //         console.log("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data);
+       //     }
+       // }
+
+        // Transaction error callback
+        var errorCB = function (err) {
+            console.log("Error processing SQL: "+err.code);
+        }
+
+    };
+};
+
+var album = function () {
+    var db = sqlite_db();
+    return {
+        list_photo : function (cb) {
+
+        },
+        list_tag : function (cb) {
+
+        },
+        insert_photo : function (path, create_date, tag) {
+        
+        }
+    };
+};
+
 var app = {
     // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    init: function() {
+        db.sqlite_db();
     }
 };
